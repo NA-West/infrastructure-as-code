ca84db25c4c67ce43ef52f925a2bb1ad49bba9bb
NA-West <44859123+NA-West@users.noreply.github.com>
2018-11-10 19:07:47 -0700

    change to outputs

--- zabbix/terraform/outputs.tf
+++ zabbix/terraform/outputs.tf
@@ -1,3 +1,3 @@
 output "zabbix_public_ip" {
-  value = "${google_compute_instance.raddit.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "${google_compute_instance.zabbix.network_interface.0.access_config.0.assigned_nat_ip}"
 }
